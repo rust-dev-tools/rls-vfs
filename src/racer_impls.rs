03bea2e6a650b13be7e404f20f43c095c49cc9a1
Nick Cameron <ncameron@mozilla.com>
2017-05-29 18:15:40 +1200

    racer impl change for binary files

--- src/racer_impls.rs
+++ src/racer_impls.rs
@@ -3,11 +3,14 @@ extern crate racer;
 use std::io;
 use std::path::Path;
 
-use Vfs;
+use {Vfs, FileContents, Error};
 
 impl racer::FileLoader for Vfs {
     fn load_file(&self, path: &Path) -> io::Result<String> {
-        self.0.load_file(path)
-            .map_err(|err| io::Error::new(io::ErrorKind::Other, err))
+        match self.0.load_file(path) {
+            Ok(FileContents::Text(t)) => Ok(t),
+            Ok(FileContents::Binary(_)) => Err(io::Error::new(io::ErrorKind::Other, Error::BadFileKind)),
+            Err(err) => Err(io::Error::new(io::ErrorKind::Other, err)),
+        }
     }
 }
