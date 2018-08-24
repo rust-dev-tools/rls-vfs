9cae1ae343f029fa3b10a60b9383742821f10f0c
kngwyu <yuji.kngw.80s.revive@gmail.com>
2018-08-24 19:47:45 +0900

    Avoid code duplication

--- benches/bench.rs
+++ benches/bench.rs
@@ -7,12 +7,12 @@ use rls_span::{Column, Position, Row, Span};
 use rls_vfs::Change;
 use std::fs;
 use std::io::prelude::*;
-use std::path::Path;
+use std::path::{Path, PathBuf};
 
 struct EmptyUserData;
 type Vfs = rls_vfs::Vfs<EmptyUserData>;
 
-fn add_file(vfs: &mut Vfs, path: &Path) {
+fn add_file(vfs: &Vfs, path: &Path) {
     let mut buf = String::new();
     let mut file = fs::File::open(path).unwrap();
     file.read_to_string(&mut buf).unwrap();
@@ -23,7 +23,7 @@ fn add_file(vfs: &mut Vfs, path: &Path) {
     vfs.on_changes(&[change]).unwrap();
 }
 
-fn make_replace(path: &Path, start_line: usize) -> Change {
+fn make_change_(path: &Path, start_line: usize, interval: usize) -> Change {
     const LEN: usize = 10;
     let txt = unsafe { std::str::from_utf8_unchecked(&[b' '; 100]) };
     let start = Position::new(
@@ -31,7 +31,7 @@ fn make_replace(path: &Path, start_line: usize) -> Change {
         Column::new_zero_indexed(0),
     );
     let end = Position::new(
-        Row::new_zero_indexed((start_line + LEN) as u32),
+        Row::new_zero_indexed((start_line + interval) as u32),
         Column::new_zero_indexed(0),
     );
     let buf = (0..LEN).map(|_| txt.to_owned() + "\n").collect::<String>();
@@ -42,29 +42,24 @@ fn make_replace(path: &Path, start_line: usize) -> Change {
     }
 }
 
+fn make_replace(path: &Path, start_line: usize) -> Change {
+    make_change_(path, start_line, 10)
+}
+
 fn make_insertion(path: &Path, start_line: usize) -> Change {
-    let txt = unsafe { std::str::from_utf8_unchecked(&[b' '; 100]) };
-    let start = Position::new(
-        Row::new_zero_indexed(start_line as u32),
-        Column::new_zero_indexed(0),
-    );
-    let end = Position::new(
-        Row::new_zero_indexed((start_line + 1) as u32),
-        Column::new_zero_indexed(0),
-    );
-    let buf = (0..10).map(|_| txt.to_owned() + "\n").collect::<String>();
-    Change::ReplaceText {
-        span: Span::from_positions(start, end, path),
-        len: None,
-        text: buf,
-    }
+    make_change_(path, start_line, 1)
+}
+
+fn prepare() -> (Vfs, PathBuf) {
+    let vfs = Vfs::new();
+    let lib = Path::new("resources").join("path.rs").to_owned();
+    add_file(&vfs, &lib);
+    (vfs, lib)
 }
 
 #[bench]
 fn replace_front(b: &mut test::Bencher) {
-    let mut vfs = Vfs::new();
-    let lib = Path::new("resources").join("path.rs").to_owned();
-    add_file(&mut vfs, &lib);
+    let (vfs, lib) = prepare();
     b.iter(|| {
         for _ in 0..10 {
             let change = make_replace(&lib, 0);
@@ -75,9 +70,7 @@ fn replace_front(b: &mut test::Bencher) {
 
 #[bench]
 fn replace_mid(b: &mut test::Bencher) {
-    let mut vfs = Vfs::new();
-    let lib = Path::new("resources").join("path.rs").to_owned();
-    add_file(&mut vfs, &lib);
+    let (vfs, lib) = prepare();
     b.iter(|| {
         for _ in 0..10 {
             let change = make_replace(&lib, 2000);
@@ -88,9 +81,7 @@ fn replace_mid(b: &mut test::Bencher) {
 
 #[bench]
 fn replace_tale(b: &mut test::Bencher) {
-    let mut vfs = Vfs::new();
-    let lib = Path::new("resources").join("path.rs").to_owned();
-    add_file(&mut vfs, &lib);
+    let (vfs, lib) = prepare();
     b.iter(|| {
         for _ in 0..10 {
             let change = make_replace(&lib, 4000);
@@ -101,9 +92,7 @@ fn replace_tale(b: &mut test::Bencher) {
 
 #[bench]
 fn insert_front(b: &mut test::Bencher) {
-    let mut vfs = Vfs::new();
-    let lib = Path::new("resources").join("path.rs").to_owned();
-    add_file(&mut vfs, &lib);
+    let (vfs, lib) = prepare();
     b.iter(|| {
         for _ in 0..10 {
             let change = make_insertion(&lib, 0);
@@ -114,9 +103,7 @@ fn insert_front(b: &mut test::Bencher) {
 
 #[bench]
 fn insert_mid(b: &mut test::Bencher) {
-    let mut vfs = Vfs::new();
-    let lib = Path::new("resources").join("path.rs").to_owned();
-    add_file(&mut vfs, &lib);
+    let (vfs, lib) = prepare();
     b.iter(|| {
         for _ in 0..10 {
             let change = make_insertion(&lib, 2000);
@@ -127,9 +114,7 @@ fn insert_mid(b: &mut test::Bencher) {
 
 #[bench]
 fn insert_tale(b: &mut test::Bencher) {
-    let mut vfs = Vfs::new();
-    let lib = Path::new("resources").join("path.rs").to_owned();
-    add_file(&mut vfs, &lib);
+    let (vfs, lib) = prepare();
     b.iter(|| {
         for _ in 0..10 {
             let change = make_insertion(&lib, 4000);
